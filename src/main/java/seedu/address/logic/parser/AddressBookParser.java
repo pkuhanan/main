b42c92b5ab89f41d0902797089a707121d98b0ae
Artsiom Skliar <artsiom.sk@gmail.com>
2018-03-20 22:38:16 +0800

    Balance feature documentation, source code, and tests included.  (#33)
    * Updated userguide to include balance feature
    
    * Updated Coming in V2.0 features
    
    * Updated Developper Guide for Balance feature
    
    * Updated Userguide. Included new v2.0 feature
    
    * Added features coming in V2.0
    
    * Updated summary of commands including v2.0 features
    
    * Deleted Learning Outcome Page
    
    * Modified documentation to exclude references to AddressBook
    
    * Added Running Test Section
    
    * Added Interest Rate feature to UserGuide
    
    * Added User Stories according to v2.0
    
    * Updated user stories
    
    * Changed Double to double in Money class
    
    * Changed reference Addressbook to Travel Banker in Money class
    
    * Updated Userguide Story for Select command to load google search for nearby ATM
    
    * Added Feature contribution section and modified Value proposition following TA's comments
    
    * Added feature contribution about select method
    
    * Balance command now works
    
    * Added two decimal places formatting for balance feature
    
    * Improved formatting
    
    * Preparing to write test
    
    * Added test for Balance Comand
    
    * Updated Comamnd Test
    
    * Started to describe balance feature in developper guide
    
    * Added Class Diagram in Balance Implementation
    
    * Included Sequence Diagram for documentation of Balance feature
    
    * Sequence Diagram updated
    
    * Added activity diagram for balance command
    
    * Updated balance feature documentation
    
    * Finished documentation for balance command
    
    * Added encryption tag to pass gradle
    
    * Added encryption tag to pass gradle
    
    * style error
    
    * fixing gradle
    
    * Style changes to pass travis
    
    * Fixing checkstyle for travis
    
    * Updated Balance Command styles to pass travis

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.BalanceCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
@@ -124,6 +125,12 @@ public class AddressBookParser {
 
         case RemoveTagCommand.COMMAND_SHORTCUT:
             return new RemoveTagCommandParser().parse(arguments);
+
+        case BalanceCommand.COMMAND_SHORTCUT:
+            return new BalanceCommand();
+
+        case BalanceCommand.COMMAND_WORD:
+            return new BalanceCommand();
 
         case MaxCommand.COMMAND_WORD:
             return new MaxCommand();
