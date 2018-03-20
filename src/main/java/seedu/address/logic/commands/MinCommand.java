5edb4a601d46d5f35a43f7cf0b70fa80dc4bb5c6
Artsiom Skliar <artsiom.sk@gmail.com>
2018-03-21 00:39:58 +0800

    Min Feature Updated - Corrected to not select anyone if no money is owed.  (#49)
    * Updated userguide to include balance feature
    
    * Updated Coming in V2.0 features
    
    * Updated Developper Guide for Balance feature
    
    * Updated Userguide. Included new v2.0 feature
    
    * Added features coming in V2.0
    
    * Updated summary of commands including v2.0 features
    
    * Deleted Learning Outcome Page
    
    * Modified documentation to exclude references to AddressBook
    
    * Added Running Test Section
    
    * Added Interest Rate feature to UserGuide
    
    * Added User Stories according to v2.0
    
    * Updated user stories
    
    * Changed Double to double in Money class
    
    * Changed reference Addressbook to Travel Banker in Money class
    
    * Updated Userguide Story for Select command to load google search for nearby ATM
    
    * Added Feature contribution section and modified Value proposition following TA's comments
    
    * Added feature contribution about select method
    
    * Balance command now works
    
    * Added two decimal places formatting for balance feature
    
    * Improved formatting
    
    * Preparing to write test
    
    * Added test for Balance Comand
    
    * Updated Comamnd Test
    
    * Started to describe balance feature in developper guide
    
    * Added Class Diagram in Balance Implementation
    
    * Included Sequence Diagram for documentation of Balance feature
    
    * Sequence Diagram updated
    
    * Added activity diagram for balance command
    
    * Updated balance feature documentation
    
    * Finished documentation for balance command
    
    * Added encryption tag to pass gradle
    
    * Added encryption tag to pass gradle
    
    * style error
    
    * fixing gradle
    
    * Style changes to pass travis
    
    * Fixing checkstyle for travis
    
    * Updated Balance Command styles to pass travis
    
    * Added Usage message to BalanceCommand & incorporated MinCommand
    
    * Added find Min Command
    
    * Updated documentation
    
    * Gradle bulid passed
    
    * Fixed MinFeature to not display anything if all balances are positive
    
    * Gradle Build passed
    
    * Removed trailing whitespace
    
    * Removed trailing whitespace

--- src/main/java/seedu/address/logic/commands/MinCommand.java
+++ src/main/java/seedu/address/logic/commands/MinCommand.java
@@ -15,15 +15,23 @@ public class MinCommand extends Command {
 
     public static final String COMMAND_WORD = "min";
     public static final String COMMAND_SHORTCUT = "mn";
-    public static final String MESSAGE_SUCCESS = "The contact to which you owe the most money is: ";
-
+    public static final String MESSAGE_SUCCESS_FOUND = "The contact to which you owe the most money is: ";
+    public static final String MESSAGE_SUCCESS_NO_RESULT = "Good news! You don't owe any money.";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds the person to which you owe the most money ";
+    private CommandResult result;
 
+    public CommandResult getResult() {
+        return result;
+    }
+
+    public void setResult(CommandResult result) {
+        this.result = result;
+    }
     @Override
     public CommandResult execute() {
         List<Person> lastShownList = model.getFilteredPersonList();
         Index index = Index.fromZeroBased(0);
-        double lowestDebt = lastShownList.get(0).getMoney().balance;
+        double lowestDebt = 0.0;
 
         for (int i = 0; i < lastShownList.size(); i++) {
             Person person = lastShownList.get(i);
@@ -31,9 +39,13 @@ public class MinCommand extends Command {
                 index = Index.fromZeroBased(i);
                 lowestDebt = person.getMoney().balance;
             }
+            if (lowestDebt == 0.0) {
+                result = new CommandResult(MESSAGE_SUCCESS_NO_RESULT);
+            } else {
+                EventsCenter.getInstance().post(new JumpToListRequestEvent(index));
+                result = new CommandResult(MESSAGE_SUCCESS_FOUND + lastShownList.get(index.getZeroBased()).getName());
+            }
         }
-
-        EventsCenter.getInstance().post(new JumpToListRequestEvent(index));
-        return new CommandResult(MESSAGE_SUCCESS + lastShownList.get(index.getZeroBased()).getName());
+        return result;
     }
 }
